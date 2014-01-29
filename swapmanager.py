ac9968a3536b3fef8d280ce76f3181362746065f
Craig Knott <craig.knott92@gmail.com>
2014-01-29 11:58:07 +1000

    Update swapmanager.py

--- swapmanager.py
+++ swapmanager.py
@@ -1,27 +1,3 @@
-"""
-The MIT License (MIT)
-
-Copyright (c) 2014 Craig C Knott
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
-
 from swap.SwapInterface import SwapInterface
 from MQTT import MQTT
 
@@ -89,12 +65,7 @@ class SwapManager(SwapInterface):
                 e = sys.exc_info()[0]
                 print ("<publishData> Error: %s" % e )
                 
-	    # Check if mosquito accepted the publish or not.
-    	    if (result == 0):
-    	        print "PUBLISH SUCCESS: " + str(pub_data)
-    	    else:
-                print "PUBLISH FAILED: " + str(pub_data)
-    	        #sys.exit(2)        
+	          
 
     def shell_command(self, command):
         """
